a78d5978e363f0a80c9a488c82d804dcb99b5fae
Memo Chou <memochou1993@gmail.com>
2022-05-10 16:48:32 +0800

    Update Staking contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -18,32 +18,32 @@ contract Staking is ERC20, Ownable {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
-    function createStake(uint256 _stake)
+    function createStake(uint256 _amount)
         public
     {
-        _burn(msg.sender, _stake);
+        _burn(msg.sender, _amount);
         if (stakes[msg.sender] == 0) {
             addStakeholder(msg.sender);
         }
-        stakes[msg.sender] = stakes[msg.sender].add(_stake);
+        stakes[msg.sender] += _amount;
     }
 
-    function removeStake(uint256 _stake)
+    function removeStake(uint256 _amount)
         public
     {
-        stakes[msg.sender] = stakes[msg.sender].sub(_stake);
+        stakes[msg.sender] -= _amount;
         if (stakes[msg.sender] == 0) {
             removeStakeholder(msg.sender);
         }
-        _mint(msg.sender, _stake);
+        _mint(msg.sender, _amount);
     }
 
-    function stakeOf(address _address)
+    function stakeOf(address _stakeholder)
         public
         view
         returns (uint256)
     {
-        return stakes[_address];
+        return stakes[_stakeholder];
     }
 
     function totalStakes()
@@ -53,49 +53,49 @@ contract Staking is ERC20, Ownable {
     {
         uint256 _totalStakes = 0;
         for (uint256 i = 0; i < stakeholders.length; i += 1) {
-            _totalStakes = _totalStakes.add(stakes[stakeholders[i]]);
+            _totalStakes += stakes[stakeholders[i]];
         }
         return _totalStakes;
     }
 
-    function isStakeholder(address _address)
+    function isStakeholder(address _stakeholder)
         public
         view
         returns (bool, uint256)
     {
         for (uint256 i = 0; i < stakeholders.length; i += 1) {
-            if (_address == stakeholders[i]) {
+            if (_stakeholder == stakeholders[i]) {
                 return (true, i);
             }
         }
         return (false, 0);
     }
 
-    function addStakeholder(address _address)
+    function addStakeholder(address _stakeholder)
         public
     {
-        (bool _isStakeholder, ) = isStakeholder(_address);
+        (bool _isStakeholder, ) = isStakeholder(_stakeholder);
         if (!_isStakeholder) {
-            stakeholders.push(_address);
+            stakeholders.push(_stakeholder);
         }
     }
 
-    function removeStakeholder(address _address)
+    function removeStakeholder(address _stakeholder)
         public
     {
-        (bool _isStakeholder, uint256 s) = isStakeholder(_address);
+        (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
         if (_isStakeholder) {
             stakeholders[s] = stakeholders[stakeholders.length - 1];
             stakeholders.pop();
         }
     }
 
-    function rewardOf(address _address)
+    function rewardOf(address _stakeholder)
         public
         view
         returns (uint256)
     {
-        return rewards[_address];
+        return rewards[_stakeholder];
     }
 
     function totalRewards()
@@ -105,17 +105,17 @@ contract Staking is ERC20, Ownable {
     {
         uint256 _totalRewards = 0;
         for (uint256 i = 0; i < stakeholders.length; i += 1) {
-            _totalRewards = _totalRewards.add(rewards[stakeholders[i]]);
+            _totalRewards += rewards[stakeholders[i]];
         }
         return _totalRewards;
     }
 
-    function calculateReward(address _address)
+    function calculateReward(address _stakeholder)
         public
         view
         returns (uint256)
     {
-        return stakes[_address] / 100;
+        return stakes[_stakeholder] / 100;
     }
 
     function withdrawReward()
