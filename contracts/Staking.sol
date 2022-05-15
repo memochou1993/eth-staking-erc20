cb223de295d1c90980b6eaceb6612fb5ea0f8ec3
Memo Chou <memochou1993@gmail.com>
2022-05-15 22:52:19 +0800

    Refactor Staking contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -13,18 +13,21 @@ contract Staking is ERC20, Ownable {
 
     struct Stake {
         uint256 amount;
-        uint256 earned;
         uint256 rewardRate;
-        uint256 createdAt;
+        uint256 claimable;
+        uint256 startedAt;
     }
 
-    address[] private stakeholders;
-    // The stakes of stakeholders
-    mapping(address => Stake) private stakes;
-    // The reward rates of stake days
-    mapping(uint256 => uint256) public rewardRates;
+    struct Stakeholder {
+        address addr;
+        Stake stake;
+    }
+
+    Stakeholder[] public stakeholders;
+    mapping(address => uint256) public stakeholderIndexes;
 
     constructor() ERC20("My Token", "MTK") {
+        stakeholders.push();
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
@@ -38,107 +41,48 @@ contract Staking is ERC20, Ownable {
         return 2;
     }
 
-    function createStake(uint256 _amount)
+    function stake(uint256 _amount)
         public
     {
-        require(stakes[msg.sender].amount == 0, "Stake exists.");
-        _burn(msg.sender, _amount);
-        addStakeholder(msg.sender);
-        stakes[msg.sender] = Stake({
+        require(_amount > 0, "Cannot stake nothing");
+        uint256 index = stakeholderIndexes[msg.sender];
+        uint256 timestamp = block.timestamp;
+        if (index == 0) {
+            index = addStakeholder(msg.sender);
+        }
+        stakeholders[index].stake = Stake({
             amount: _amount,
-            earned: 0,
             rewardRate: rewardRate,
-            createdAt: block.timestamp
+            claimable: 0,
+            startedAt: timestamp
         });
+        // emit Staked
     }
 
-    function removeStake()
-        public
-    {
-        require(stakes[msg.sender].amount != 0, "Stake does not exist.");
-        uint256 _amount = stakes[msg.sender].amount;
-        delete stakes[msg.sender];
-        removeStakeholder(msg.sender);
-        _mint(msg.sender, _amount);
-    }
-
-    function stakeOf(address _stakeholder)
-        public
-        view
-        returns (Stake memory)
-    {
-        return stakes[_stakeholder];
-    }
-
-    function totalStakeAmount()
-        public
-        view
+    function addStakeholder(address _stakeholder)
+        internal
         returns (uint256)
     {
-        uint256 _amount = 0;
-        for (uint256 i = 0; i < stakeholders.length; i += 1) {
-            _amount += stakes[stakeholders[i]].amount;
-        }
-        return _amount;
+        stakeholders.push();
+        uint256 index = stakeholders.length - 1;
+        stakeholders[index].addr = _stakeholder;
+        stakeholderIndexes[_stakeholder] = index;
+        return index;
     }
 
     function isStakeholder(address _stakeholder)
         public
         view
-        returns (bool, uint256)
+        returns (bool)
     {
-        for (uint256 i = 0; i < stakeholders.length; i += 1) {
-            if (_stakeholder == stakeholders[i]) {
-                return (true, i);
-            }
-        }
-        return (false, 0);
+        return stakeholderIndexes[_stakeholder] != 0;
     }
 
-    function calculateReward(address _stakeholder)
+    function stakeAmountOf(address _stakeholder)
         public
         view
         returns (uint256)
     {
-        Stake memory _stake = stakes[_stakeholder];
-        uint256 _rewardPerSecond = _stake.amount * _stake.rewardRate / 100 / 365 / 86400;
-        return (block.timestamp - _stake.createdAt) * _rewardPerSecond - _stake.earned;
-    }
-
-    function distributeRewards()
-        public
-        onlyOwner
-    {
-        for (uint256 i = 0; i < stakeholders.length; i += 1) {
-            uint256 _reward = calculateReward(stakeholders[i]);
-            stakes[stakeholders[i]].earned += _reward;
-            _mint(msg.sender, _reward);
-        }
-    }
-
-    function setRewardRate(uint256 _rewardRate)
-        public
-        onlyOwner
-    {
-        rewardRate = _rewardRate;
-    }
-
-    function addStakeholder(address _stakeholder)
-        private
-    {
-        (bool _isStakeholder, ) = isStakeholder(_stakeholder);
-        if (!_isStakeholder) {
-            stakeholders.push(_stakeholder);
-        }
-    }
-
-    function removeStakeholder(address _stakeholder)
-        private
-    {
-        (bool _isStakeholder, uint256 i) = isStakeholder(_stakeholder);
-        if (_isStakeholder) {
-            stakeholders[i] = stakeholders[stakeholders.length - 1];
-            stakeholders.pop();
-        }
+        return stakeholders[stakeholderIndexes[_stakeholder]].stake.amount;
     }
 }
