25f6deec78a3f053b743d01a46f86ec309f7b026
Memo Chou <memochou1993@gmail.com>
2022-05-16 22:43:32 +0800

    Update Staking contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -11,16 +11,16 @@ contract Staking is ERC20, Ownable {
     uint256 public constant INITIAL_SUPPLY = 1e10;
     uint256 public rewardRate = 100;
 
+    struct Stakeholder {
+        address addr;
+        Stake[] stakes;
+    }
+
     struct Stake {
         uint256 amount;
         uint256 rewardRate;
         uint256 claimable;
         uint256 startedAt;
-    }
-
-    struct Stakeholder {
-        address addr;
-        Stake[] stakes;
     }
 
     Stakeholder[] public stakeholders;
@@ -29,6 +29,11 @@ contract Staking is ERC20, Ownable {
     constructor() ERC20("My Token", "MTK") {
         stakeholders.push();
         _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    modifier onlyStakeholder() {
+        require(isStakeholder(msg.sender), "Staking: caller is not the stakeholder");
+        _;
     }
 
     function decimals()
@@ -41,33 +46,38 @@ contract Staking is ERC20, Ownable {
         return 2;
     }
 
-    function stake(uint256 _amount)
+    function createStake(uint256 _amount)
         public
     {
-        require(_amount > 0, "Cannot stake nothing");
-        uint256 stakeholderIndex = stakeholderIndexes[msg.sender];
-        uint256 timestamp = block.timestamp;
+        require(_amount > 0, "Staking: cannot stake nothing");
+        uint256 _stakeholderIndex = stakeholderIndexes[msg.sender];
+        uint256 _timestamp = block.timestamp;
         if (!isStakeholder(msg.sender)) {
-            stakeholderIndex = addStakeholder(msg.sender);
+            _stakeholderIndex = addStakeholder(msg.sender);
         }
-        stakeholders[stakeholderIndex].stakes.push(Stake({
+        stakeholders[_stakeholderIndex].stakes.push(Stake({
             amount: _amount,
             rewardRate: rewardRate,
             claimable: 0,
-            startedAt: timestamp
+            startedAt: _timestamp
         }));
-        // emit Staked
+        _burn(msg.sender, _amount);
+        // emit StakeCreated
     }
 
-    function addStakeholder(address _stakeholder)
-        internal
-        returns (uint256)
+    function removeStake(uint256 _stakeIndex)
+        public
+        onlyStakeholder
     {
-        stakeholders.push();
-        uint256 index = stakeholders.length - 1;
-        stakeholders[index].addr = _stakeholder;
-        stakeholderIndexes[_stakeholder] = index;
-        return index;
+        uint256 _stakeholderIndex = stakeholderIndexes[msg.sender];
+        Stake[] memory _stakes = stakeholders[_stakeholderIndex].stakes;
+        require(_stakeIndex < _stakes.length, "Staking: invalid stake ID");
+        Stake memory _stake = _stakes[_stakeIndex];
+        require(_stake.amount > 0, "Staking: stake does not exist");
+        uint256 _reward = calculateReward(_stake);
+        delete stakeholders[_stakeholderIndex].stakes[_stakeIndex];
+        _mint(msg.sender, _stake.amount + _reward);
+        // emit StakeRemoved
     }
 
     function isStakeholder(address _stakeholder)
@@ -81,8 +91,30 @@ contract Staking is ERC20, Ownable {
     function stakes()
         public
         view
+        onlyStakeholder
         returns (Stake[] memory)
     {
-        return stakeholders[stakeholderIndexes[msg.sender]].stakes;
+        uint256 _stakeholderIndex = stakeholderIndexes[msg.sender];
+        return stakeholders[_stakeholderIndex].stakes;
+    }
+
+    function addStakeholder(address _stakeholder)
+        internal
+        returns (uint256)
+    {
+        stakeholders.push();
+        uint256 index = stakeholders.length - 1;
+        stakeholders[index].addr = _stakeholder;
+        stakeholderIndexes[_stakeholder] = index;
+        return index;
+    }
+
+    function calculateReward(Stake memory _stake)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 _rewardPerSecond = _stake.amount * _stake.rewardRate / 100 / 365 / 86400;
+        return (block.timestamp - _stake.startedAt) * _rewardPerSecond;
     }
 }
